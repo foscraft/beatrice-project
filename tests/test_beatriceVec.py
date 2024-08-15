b6b9fc8613389bd9bdbf43b24a954fe39adbfa7f
foscraft <anyaribari@gmail.com>
2024-08-15 15:45:44 +0300

    translation-text

--- tests/test_beatriceVec.py
+++ tests/test_beatriceVec.py
@@ -6,7 +6,7 @@ from beatricevec.main import (  # Import your BeatriceVec class from the main co
 
 # Create some sample sentences for testing
 sample_corpus = [
-    "Stratgies dapprentissage pour la postalphabtisation et lducation continue au Kenya au Nigria en Tanzanie et au RoyaumeUni",
+    "Learning strategies for post-literacy and continuing education in Kenya, Nigeria, Tanzania, and the United Kingdom",
     "Family planning and sexual behavior in the era of HIVAIDS the case of Nakuru District Kenya.. Recently the prevalence of contraceptive use has increased in Kenya.",
     "ECHIDNOPSIS GLOBOSA SPNOV ASCLEPIADACEAESTAPELIEAE FROM YEMEN. The new species Echidnopsis globosa from rocky hillsides on Limestone in the Hadramaut Region in Yemen is described and illustrated.",
 ]
